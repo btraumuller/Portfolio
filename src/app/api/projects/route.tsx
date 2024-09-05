cae8b902d7057d288bc5d0a8c21af68a219a09ac
btraumuller <btraumuller@gmail.com>
2024-09-05 00:53:43 -0400

    Remove unused files

--- src/app/api/projects/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-export async function getLatestProjects(){
-    const projects = await prisma.projects.findMany();
-    return projects;
-}
