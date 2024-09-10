8ae2b38b13dc0d6507a0e58aa80550255e7dda66
btraumuller <btraumuller@gmail.com>
2024-09-10 01:41:54 -0400

    Fixing some Styling issues on the gallery refactor, also updating resume file

--- src/app/gallery-refactor/page.tsx
+++ src/app/gallery-refactor/page.tsx
@@ -1,10 +1,15 @@
 import ImageGallery from "../components/imageGallery/imageGallery"
+import Link from "next/link";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function galleryRefactor(){
     return(
-        <div className="max-w-screen-xl mx-auto flex flex-col p-4">
-            <ImageGallery />
-        </div>
-        
+        <main>
+            <div className="max-w-screen-xl mx-auto flex flex-col px-4 pt-[8rem]">
+                <Link className="flex-sm flex" href="/projects/cG9zdDoyNg=="><FontAwesomeIcon icon={faChevronLeft} className="self-center mr-2 text-l" />Back to Project Detail</Link>
+                <ImageGallery />
+            </div>
+        </main>
     )
 }
