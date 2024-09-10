09edd6abf56845f608ac627ca4c4332402882fc4
btraumuller <btraumuller@gmail.com>
2024-09-10 16:12:52 -0400

    Removing Export from async function on projectDetail

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import {notFound} from "next/navigation";
-import { getProjectDetail } from "@/app/api/getProjectDetail";
 
 type projectDetailData = {
     title: string
@@ -20,6 +19,66 @@ type projectDetailData = {
       }
     } 
 };
+
+async function getProjectDetail(projectId:string){
+
+    
+
+    const wpGraphqlUrl = process.env.WP_GRAPHQL_URL;
+
+    if (!wpGraphqlUrl) {
+        throw new Error('WP_GRAPHQL_URL environment variable is not defined');
+    }
+
+    if (projectId.includes("%3D")){
+        projectId = projectId.replace(/%3D/g, "=");
+    }
+
+
+    try{
+        const projectDetail = await fetch(wpGraphqlUrl, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},  
+            body: JSON.stringify({
+              query: `query getProject {
+                projects(where: {in: "${projectId}"}) {
+                    nodes {
+                        title
+                        projectFields {
+                            projectDate
+                            longDescription
+                            desktopImageAltText
+                            externalLink
+                            desktopImage {
+                                node {
+                                    mediaItemUrl
+                                }
+                            }
+                        }
+                        
+                    }
+                }             
+              }
+            `})
+          });
+      
+          let json = await projectDetail.json();
+          let projectData = {
+            props: {
+              projects: json.data.projects.nodes[0]
+            }
+          }
+          
+        if (!projectDetail){
+            return notFound();
+        }
+
+        return projectData.props.projects;
+    }catch{
+        console.log('There is an error retrieving this Data');
+    }
+    
+}
 
 export default async function projectDetail({params}:{
     params:{
