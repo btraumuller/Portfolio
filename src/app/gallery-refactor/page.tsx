e79403c68af09ca0dbef7e40394cca937ac79f5c
btraumuller <btraumuller@gmail.com>
2024-12-09 18:52:40 -0500

    adding suspense to image gallery

--- src/app/gallery-refactor/page.tsx
+++ src/app/gallery-refactor/page.tsx
@@ -1,14 +1,17 @@
-import ImageGallery from "../components/ImageGallery/ImageGallery";
+import { Suspense } from "react";
 import Link from "next/link";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ImageGallery from "../components/ImageGallery/ImageGallery";
 
 export default function GalleryRefactor(){
     return(
         <main>
             <div className="max-w-screen-xl mx-auto flex flex-col px-4 pt-[8rem]">
                 <Link className="flex-sm flex" href="/projects/cG9zdDoyNg=="><FontAwesomeIcon icon={faChevronLeft} className="self-center mr-2 text-l" />Back to Project Detail</Link>
-                <ImageGallery />
+                <Suspense fallback={"...loading"}>
+                    <ImageGallery />
+                </Suspense>
             </div>
         </main>
     )
