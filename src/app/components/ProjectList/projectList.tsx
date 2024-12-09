6ffe00c755638646c82f61028186a19664f85e53
btraumuller <btraumuller@gmail.com>
2024-12-09 02:52:08 -0500

    Removing Duplicate and fixing error

--- src/app/components/ProjectList/projectList.tsx
+++ src/app/components/ProjectList/projectList.tsx
@@ -64,14 +64,14 @@ export default async function projectList({query}:{query:string}){
         ) 
     }else{
         return(
-            <div className="dm flex flex-col md:flex-row pt-8 flex flex-wrap list-column">
+            <div className="dm flex-col md:flex-row pt-8 flex flex-wrap list-column">
                  {projects.map((project, i) =>{
                     let datePosted = new Date(project.projectFields.projectDate).toLocaleDateString('default', {day:'numeric', month:'long', year:'numeric'})
                     let projectId = project.id;
                     let projectLink = '/projects/' + projectId;
                     return(
                         <Link href={projectLink} className="cursor-pointer relative mb-8 hover:relative hover:top-[-20px] project-card md:w-[30%] max-w-[400px] latest-projects-border" key={i}>
-                            <Image src={project.projectFields.thumbnailImage.node.mediaItemUrl} width={400} height={400} alt={project.projectFields.thumbnailAltText}/ >
+                            <Image src={project.projectFields.thumbnailImage.node.mediaItemUrl} width={400} height={400} alt={project.projectFields.thumbnailAltText} ></Image>
                             <div className="p-4 bg-white h-full">
                                 <p className='text-black mb-4'>{datePosted}</p>
                                 <h3 className='text-2xl text-black mb-2'>{project.title}</h3>
