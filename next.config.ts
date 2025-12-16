fc9e9dc4305cc9b7f5a3b37ec8b8616bded50ffb
btraumuller <btraumuller@gmail.com>
2025-12-15 22:44:10 -0500

    Fixing and error in deployment

--- next.config.ts
+++ next.config.ts
@@ -1,47 +1,22 @@
-
 import path from 'path';
-import { fileURLToPath } from 'url';
+import type { NextConfig } from 'next';
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
-
-const nextConfig = {
+const nextConfig: NextConfig = {
   sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-      },
-    images: {
-        dangerouslyAllowSVG: true,
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'placehold.co',
-            port:'',
-          },
-          {
-            protocol: 'http',
-            hostname: 'btraumullerportfoliocom.local',
-            port:'',
-          },
-          {
-            protocol: 'https',
-            hostname: 'btraumuller-portfolio-new.flywheelsites.com',
-            port:'',
-          },
-          {
-              protocol: 'https',
-              hostname: 'images.unsplash.com',
-              port:''
-            },
-            {
-              protocol: 'https',
-              hostname: 'plus.unsplash.com',
-              port:''
-            }
-        ]
-        
-      },
-      reactStrictMode: false,
-    
+    // adjust the folder name if needed
+    includePaths: [path.join(process.cwd(), 'src', 'app', 'globalStyles')],
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co', port: '' },
+      { protocol: 'http', hostname: 'btraumullerportfoliocom.local', port: '' },
+      { protocol: 'https', hostname: 'btraumuller-portfolio-new.flywheelsites.com', port: '' },
+      { protocol: 'https', hostname: 'images.unsplash.com', port: '' },
+      { protocol: 'https', hostname: 'plus.unsplash.com', port: '' },
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
